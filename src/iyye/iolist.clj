dcd5580c0e4f1b748cf8e5f9ad378a40ea6cf264
Sasha Yumzya <yumzya@email.com>
2017-06-18 13:21:18 +0000

    Create iolist.clj

---
+++ src/iyye/iolist.clj
@@ -0,0 +1,34 @@
+; Iyye - AI agent
+; Copyright (C) 2016-2017  Sasha Yumzya
+
+; This program is free software: you can redistribute it and/or modify
+; it under the terms of the GNU Affero General Public License as
+; published by the Free Software Foundation, either version 3 of the
+; License, or (at your option) any later version.
+
+; This program is distributed in the hope that it will be useful,
+; but WITHOUT ANY WARRANTY; without even the implied warranty of
+; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+; GNU Affero General Public License for more details.
+
+; You should have received a copy of the GNU Affero General Public License
+; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+(ns iyye.iolist
+  (:require [extras.lispychat :as lispychat]
+            [clojure.tools.logging :as log]))
+
+(def AvatarsIOList (ref ()))
+
+(defn init-avatars-list []
+  (dosync
+    (alter AvatarsIOList conj lispychat/lispy-chat-IO)))
+
+(defn init-io []
+  (letfn [(start-all [l]
+            (for [func (map :start-function l) :when func] (func)))]
+    (do [
+      (init-avatars-list)
+      (start-all @AvatarsIOList)
+      (start-all (:inputs-list @AvatarsIOList))
+      (start-all (:outputs-list @AvatarsIOList))])))
