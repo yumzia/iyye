b361ae88f80220f8f2fcfb3b52739bbee879c8c4
Sasha Yumzya <yumzya@email.com>
2017-08-06 16:03:04 +0000

    Update ioframes.clj

--- src/iyye/ioframes.clj
+++ src/iyye/ioframes.clj
@@ -17,7 +17,8 @@
 (ns iyye.ioframes
   (:require
     [clojure.tools.logging :as log]
-    [iyye.persistence :as persistence]))
+    [iyye.persistence :as persistence]
+    [iyye.resource :as resource]))
 
 (defrecord InputSources [start-function input-listeners-list])
 (defn create-InputSources [start-function input-listeners-list] (InputSources. start-function input-listeners-list))
@@ -26,15 +27,14 @@
 (defrecord IO [name start-function inputs-list outputs-list])
 
 (defn create-IO [name start-fumction inputs-list outputs-list] (IO. name start-fumction inputs-list outputs-list))
-; list of IOs
 
 (defn process-input [IO input]
   (log/info (str "in: " (:name IO) input))
   (for [func (map :input-listeners-list IO)
         :when func] (func input))
-  (persistence/write-io-to-db IO :input input))
+  (persistence/write-io-to-db @resource/num-days IO :input input))
 
 (defn process-output [IO output]
   (log/info (str "out:" (:name IO) output))
   ((first (:outputs-list (:outputs-list IO))) output)
-  (persistence/write-io-to-db IO :output output))
+  (persistence/write-io-to-db @resource/num-days IO :output output))
