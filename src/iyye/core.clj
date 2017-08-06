316a3351fbc3ea7039e07ecd69f5b19fb7d9e41a
Sasha Yumzya <yumzya@email.com>
2017-08-06 11:10:08 +0000

    added command line parsing

--- src/iyye/core.clj
+++ src/iyye/core.clj
@@ -17,27 +17,15 @@
 (ns iyye.core
   (:require [iyye.iolist :as iolist]
             [iyye.resource :as resource]
-            [clojure.tools.logging :as log]))
-
-(def load-state? false)
-
-(defn- load-state []
-  ;  (dorun (process-wakeup))
-  )
-
-(defn- init-state []
-   (log/info "first day")
-)
+            [iyye.persistence :as persistence]
+            [clojure.tools.logging :as log]
+            [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as str]))
 
 (def DAY_CHECK_TIME_MILLIS 1000)
 
 ; wake sleep state machine
-(defn main-function []
-  ;  (iolist/init-io)
-
-    (if load-state?
-      (load-state)
-      (init-state))
+(defn main-loop []
     (while true
       (let [current-day (resource/create-day)]
         (resource/start-day current-day)
@@ -54,7 +42,75 @@
 
           (resource/stop-night current-night)))))
 
+(defn- load-state [name]
+  (log/info "first day load")
+  (println "first day load !!")
+  (persistence/load-current-iyye name)
+  (resource/load-day!)
+  (iolist/load-io-state))
+
+(defn- init-state [name]
+  (log/info "first day")
+  (println "first day !!")
+  (persistence/set-current-iyye name))
+
+(defn usage [options-summary]
+  (->> ["Usage: iyye action/option"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  list    list IYYE"
+        "  load     load IYYE"
+        "  create   create IYYE"
+        ""]
+       (str/join \newline)))
+
+(def cli-options
+  ;; An option with a required argument
+  [["-n" "--name IYYE" "IYYE name"
+    :default "new"]
+   ;   :validate [#(iyye-exists? %) "Does not exist"]]
+    ["-h" "--help"]])
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (str/join \newline errors)))
+
+(defn validate-args
+  "Validate command line arguments. Either return a map indicating the program
+  should exit (with a error message, and optional ok status), or a map
+  indicating the action the program should take and the options provided."
+  [args]
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+    ;       (println "opt:" options "args:" arguments "err:" errors "summary:" summary)
+    (cond
+      (:help options) ; help => exit OK with usage summary
+      {:exit-message (usage summary) :ok? true}
+
+      errors ; errors => exit with description of errors
+      {:exit-message (error-msg errors)}
+
+      (and (= 1 (count arguments))
+           (#{"create" "list" "load"} (first arguments)))
+      {:action (first arguments) :options options}
+      :else ; failed custom validation => exit with usage summary
+      {:exit-message (usage summary)})))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn -main [& args]
+  (let [{:keys [action options exit-message ok?]} (validate-args args)]
+    (if exit-message
+      (exit (if ok? 0 1) exit-message)
+      (case action
+        "list"   (doall (println (persistence/list-iyye) ) (exit 0 ""))
+        "create" (if (persistence/iyye-exists? (:name options)) (exit -1 "Already exists") (init-state (:name options)))
+        "load"  (if (persistence/iyye-exists? (:name options)) (load-state (:name options)) (exit -1 "Does not exist")))))
+
+  (doall  (iolist/init-io))
 ; Start in a thread so that I can use REPL too
-(defn -main []
-  (-> (Thread. main-function) .start)
-  (iolist/init-io))
+  (-> (Thread. main-loop) .start))
