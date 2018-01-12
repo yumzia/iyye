706a31c7e4164543386df67820201c86a9ca0bc3
Sasha Yumzya <yumzya@email.com>
2018-01-12 08:22:12 +0000

    Update words.clj

--- src/iyye/subcon/knowledge/words.clj
+++ src/iyye/subcon/knowledge/words.clj
@@ -22,92 +22,118 @@
     ;[iyye.subcon.knowledge.relation :as relation]
     ))
 
-(def action-words (ref []))
-(def noun-words (ref []))
+(def action-words (ref {}))
+(def noun-words (ref {}))
 ;(def types-words (ref []))
 ;(def relations-words (ref []))
-(def instances-words (ref []))
-(def adjective-words (ref []))
+;(def instances-words (ref []))
+;(def adjective-words (ref []))
 
 (defrecord Iyye_ModalPredicate [AccordingTo When Time Prob])
-(defrecord Iyye_Atom [Name Uname])
-(defrecord Iyye_Relation [atom Predicate Types Function Data])
+(defrecord Iyye_Atom [Name Uname Builtin])
+(defrecord Iyye_Relation [atom Predicate Types Function PredicateFunction Data])
 (defrecord Iyye_Type [atom Relations])
 (defrecord Iyye_Instance [atom type])
 
-(defn create-iyye-atom [name words-list]
-  (let [Time (persistence/current-time-to-string)
-        uname (str name (count words-list))
-        atom (->Iyye_Atom name uname)]
+(defn create-iyye-atom [name words-list & builtin]
+  (let [uname (str name (count words-list))
+        b (if (nil? builtin) false (first builtin))
+        atom (->Iyye_Atom name uname b)]
     atom))
 
-(defn create-iyye-type [Name]
-  (let [type-atom (create-iyye-atom Name noun-words)
+(defn create-iyye-type [Name & builtin]
+  (let [type-atom (create-iyye-atom Name noun-words builtin)
         type (->Iyye_Type type-atom [])]
     type))
 
-(defn create-iyye-relation [Name AccordingTo Reason When Types Function]
-  (let [action-atom (create-iyye-atom Name @noun-words)
-        relation (->Iyye_Relation action-atom Reason Types Function [])]
+(defn create-iyye-relation [Name Predicate Types Function PredicateFunction & builtin]
+  (let [action-atom (create-iyye-atom Name @noun-words builtin)
+        relation (->Iyye_Relation action-atom Predicate Types Function PredicateFunction [])]
     relation))
 
 (defn create-iyye-instance [values])
 
 (defn load-iyye-atom-from-db [uname])
 
+(defn load-iyye-atoms-from-db [name & [dbname]])
+
 (defn load-iyye-types-from-db [name]
+  (load-iyye-atoms-from-db name "types"))
+
+(defn save-iyye-type-to-db [type]
   )
 
 (defn load-iyye-relations-from-db [name]
-  )
+  (load-iyye-atoms-from-db name "relations"))
+
+(defn get-iyye-atoms [name builtins dbname]
+  (let [builtin-types #(for [word @builtins :when (= (:Name word) name)] word)
+        loaded-types (load-iyye-atoms-from-db name dbname)]
+    (if (empty? builtin-types)
+      (if (empty? loaded-types)
+        :UNKNOWN
+        loaded-types)
+      (if (empty? loaded-types)
+        builtin-types
+        (apply conj builtin-types loaded-types)))))
 
 (defn get-iyye-types [name]
-  (let [builtin-types #(for [word @noun-words :when (= (:Name word) name)] word)
-        loaded-types (load-iyye-types-from-db name)]
-      (if (empty? builtin-types)
-        (if (empty? loaded-types)
-          :UNKNOWN
-          loaded-types)
-        (if (empty? loaded-types)
-          builtin-types
-          (apply conj builtin-types loaded-types)))))
+  (get-iyye-atoms name noun-words "types"))
 
 (defn set-iyye-type! [type]
-  )
+  (let [uname (:Uname (:atom type))
+        builtin (:Builtin (:atom type))]
+(do (dosync (alter noun-words assoc @noun-words uname type))
+    (when builtin
+      (save-iyye-type-to-db type)))
+    ))
+
+; (defn get-iyye-relations [name]
+  ; (let [builtin-relations #(for [word @action-words :when (= (:Name word) name)] word)
+  ;       loaded-relations (load-iyye-relations-from-db name)]
+  ; (if (empty? builtin-relations)
+  ; (if (empty? loaded-relations)
+  ;       :UNKNOWN
+  ;      loaded-relations)
+  ; (if (empty? loaded-relations)
+  ;        builtin-relations
+  ;        (apply conj builtin-relations loaded-relations)))))
 
 (defn get-iyye-relations [name]
-  (let [builtin-relations #(for [word @action-words :when (= (:Name word) name)] word)
-        loaded-relations (load-iyye-relations-from-db name)]
+  (let [builtin-relations #(for [word @action-words :when (= (:Name word) name)] word)]
     (if (empty? builtin-relations)
-      (if (empty? loaded-relations)
-        :UNKNOWN
-        loaded-relations)
-      (if (empty? loaded-relations)
-        builtin-relations
-        (apply conj builtin-relations loaded-relations)))))
+      :UNKNOWN
+      builtin-relations)))
 
 (defn check-params [action params]
   (let [act-params (:Types action)]
-  (compare act-params (map #(:Name (:atom %)) params))))
+    (compare act-params (map #(:Name (:atom %)) params))))
 
-;(dosync (alter types-words conj type))
-;(persistence/write-noun-to-db (into {} type))
+  ;(dosync (alter types-words conj type))
+  ;(persistence/write-noun-to-db (into {} type))
 
 (defn apply-relation [relation params]
   (when (check-params relation params)
     ((:Function relation) params)))
 
-(defn action [cmd params IO]
+(defn apply-query [relation params]
+  (when (check-params relation params)
+    ((:PredicateFunction relation) params)))
+
+(defn- run-action [cmd params IO func]
   (let [actions-list (get-iyye-relations cmd)
         params-list (map get-iyye-types params)]
     (case (count actions-list)
       0 (ioframes/process-output IO (str "failed to parse: no matching action to " cmd))
       1 (let [action (first actions-list)]
-          (when (not (apply-relation action params-list))
+          (when (not (func action params-list))
             (ioframes/process-output IO (str "failed to parse: types mismatch " cmd ": " action ":" params))))
-
       (let [matching-actions (for [action @actions-list :when (check-params action params-list)] action)]
         (if (empty? matching-actions)
           (ioframes/process-output IO (str "failed to parse: no many matched parameters to " cmd ": " actions-list))
-          ((:Function (first matching-actions)) params-list))))))                     ; FIXME Yumzya first
+          (func (first matching-actions) params-list)))))) ; FIXME Yumzya first
 
+(defn action [cmd params IO]
+  (if (= \? (last cmd))
+    (run-action (subs cmd 0 (dec (count cmd))) params IO apply-query)
+    (run-action cmd params IO apply-relation)))
